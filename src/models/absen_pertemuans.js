89faabf2c9592ff838e05bf9c511b14658e02052
iqbalyo <iqbalmustakim734@gmail.com>
2025-01-31 15:40:48 +0700

    n

--- src/models/absen_pertemuans.js
+++ src/models/absen_pertemuans.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
   }
   absen_pertemuans.init({
     id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       primaryKey: true, // Menandakan bahwa ini adalah primary key
       autoIncrement: true // Untuk auto increment jika diperlukan
     },
@@ -46,20 +46,20 @@ module.exports = (sequelize, DataTypes) => {
     ruang: DataTypes.STRING,
     waktu: DataTypes.STRING,
     tanggal_kuliah: DataTypes.STRING,
-    lama_kuliah: DataTypes.INTEGER,
-    pekan_ke: DataTypes.INTEGER,
-    pertemuan_ke: DataTypes.INTEGER,
+    lama_kuliah: DataTypes.STRING,
+    pekan_ke: DataTypes.STRING,
+    pertemuan_ke: DataTypes.STRING,
     bahasan: DataTypes.STRING,
     lokasi: DataTypes.STRING,
     device_id: DataTypes.STRING,
     user_agent: DataTypes.STRING,
-    waktu_tunggu: DataTypes.INTEGER,
-    waktu_presensi: DataTypes.INTEGER,
+    waktu_tunggu: DataTypes.STRING,
+    waktu_presensi: DataTypes.STRING,
     buka_at: DataTypes.STRING,
     buka_by: DataTypes.STRING,
     buka_from: DataTypes.STRING,
     tutup_at: DataTypes.STRING,
-    libur: DataTypes.BOOLEAN,
+    libur: DataTypes.STRING,
     batas_lokasi: DataTypes.STRING,
     kirim_notifikasi: DataTypes.BOOLEAN,
     kode: DataTypes.STRING,
@@ -68,8 +68,8 @@ module.exports = (sequelize, DataTypes) => {
     semester: DataTypes.STRING,
     periode: DataTypes.STRING,
     created_by: DataTypes.STRING,
-    created_at: DataTypes.DATE,
-    updated_at: DataTypes.DATE
+    created_at: DataTypes.STRING,
+    updated_at: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'absen_pertemuans',
