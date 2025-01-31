2cd1f8d8e13a303a22f75ad3e8f6ddc2ab93fc14
iqbalyo <iqbalmustakim734@gmail.com>
2025-01-31 14:43:38 +0700

    n

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const config = {
   development: {
-    username: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    host: process.env.PGHOST,
+    username: "db_siakbaak_owner",
+    password: "YZ2p7DLtwaez",
+    database: "db_siakbaak",
+    host: "ep-damp-fog-a10p5bcq-pooler.ap-southeast-1.aws.neon.tech",
     dialect: "postgres",
     dialectOptions: {
       ssl: {
