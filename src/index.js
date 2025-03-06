27ab18a4a4d846ddf86f10ac0b2b3c47e688f205
iqbalyo <iqbalmustakim734@gmail.com>
2025-03-06 15:11:01 +0700

    oo

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const helmet = require("helmet");
 const router = require("./router/router");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 // Middleware
-app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({
-    origin: ["https://fe-deploy-olive.vercel.app", "http://localhost:5173"],
-    credentials: true  // Jika menggunakan cookies atau JWT
+    origin: ["https://fe-deploy-olive.vercel.app", "http://localhost:5173"]
 }));
 
 // Routes
@@ -22,7 +19,7 @@ app.use(router);
 
 // Fallback untuk rute yang tidak ditemukan
 app.use((req, res) => {
-    res.json({
+    res.send({
         message: "Hallo 👋",
         status: "Server ready 🚀",
     });
@@ -30,5 +27,6 @@ app.use((req, res) => {
 
 // Server start
 app.listen(port, () => {
-    console.log(`🚀 Server running at http://localhost:${port}`);
-});
+    console.log(`Server running at http://localhost:${port}`);
+}); 
+
