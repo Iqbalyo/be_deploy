5ec58e8fec639c199d09b0d072668e7a09004bf9
iqbalyo <iqbalmustakim734@gmail.com>
2025-03-13 21:56:18 +0700

    aa

--- src/router/router.js
+++ src/router/router.js
@@ -16,7 +16,7 @@ const JadwalPerkuliahan2023 = require("./perkuliahan2023")
 
 
 
-router.use("/monitoring/unama/v1/informasi-kehadiran", informasiKehadiran); // Gunakan router baru//todo1
+router.use("/monitoring/unama/v1/informasi-kehadiran", informasiKehadiran); //todo1
 router.use("/monitoring/unama/v1/jadwalHariIni", jadwalKuliahHariIni);
 router.use("/monitoring/unama/v1/jadwalKuliah", jadwalKuliah);
 router.use("/monitoring/unama/v1/jadwalkuliahunama", JadwalPerkuliahan2023);
@@ -27,8 +27,8 @@ router.use("/monitoring/unama/v1/jurusan", jurusan)
 router.use("/monitoring/unama/v1/aktivitas_kuliahs", semester)
 router.use("/monitoring/unama/v1/mahasiswa", mahasiswa)
 // router.use("/monitoring/unama/v1/jadwal", schedule)
-// router.use("/monitoring/unama/v1/jadwalbelajar", schedules); // Gunakan router jadwal kuliah
-router.use("/monitoring/unama/v1/detailkehadiran", detailKehadiran); // Gunakan rute baru
+// router.use("/monitoring/unama/v1/jadwalbelajar", schedules); 
+router.use("/monitoring/unama/v1/detailkehadiran", detailKehadiran); 
 
 
 //todo jika,di router sudah bikin urlny,parameternya cuman boleh 1
